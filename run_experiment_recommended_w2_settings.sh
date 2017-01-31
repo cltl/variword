d1e34f320461b2becde1ae24eb3307501f841a86
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-31 09:04:59 +0100

    adding script with recommended settings

---
+++ run_experiment_recommended_w2_settings.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+
+# Generate unique id to distinguish between runs:
+start=$(date "+%F-%T")
+UUID='run'.$start
+IN=$1
+DIR=$2
+MODELDIR=$DIR/$UUID
+RESDIR=$3
+
+mkdir -p $MODELDIR
+mkdir -p $RESDIR$UUID
+
+echo "./run_experiment_recommend_w2_s" 
+
+#1. create pairs (win=2, dyn=dirty, sub=10^-5) and counts
+./clean_data_2_sub_dirty_pairs_and_counts.sh $IN $MODELDIR 2 1e-5
+
+#2 create ppmi and svd models (neg=5, cds=0.75, eig=0, dim=500)
+# get_ppmi_and_svd_print_out.sh dir cds dim neg eig
+./get_ppmi_and_svd_print_out.sh $MODELDIR 0.75 500 5 0.0
+
+#3 get results for ppmi and svd models
+./get_results_ppmi_svd.sh $UUID $MODELDIR $RESDIR 1 0.0 
+
+echo 'Done evaluating ppmi and svd'
+
+#4 initiate embeddings for word2vec
+./initiate_word2vec.sh $MODELDIR/counts.words.vocab $MODELDIR/pinit1
+./initiate_word2vec.sh $MODELDIR/counts.words.vocab $MODELDIR/pinit2
+./initiate_word2vec.sh $MODELDIR/counts.words.vocab $MODELDIR/pinit3
+
+#5 create and evaluate word2vec with all 3 random initiations and svd
+# settings: size=500, neg=1, iters=50
+# create_word2vec_with_init.sh pairs voc init out size neg iter
+./create_word2vec_with_init.sh $MODELDIR/pairs $MODELDIR/counts.words.vocab $MODELDIR/pinit1 $MODELDIR/sgns_rand_pinit1 500 1 50
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit1.words
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit1.contexts
+./get_results_sgns_model.sh $UUID $MODELDIR/sgns_rand_pinit1 $RESDIR pinit1
+
+
+echo 'Pinit 1 has been evaluated'
+
+./create_word2vec_with_init.sh $MODELDIR/pairs $MODELDIR/counts.words.vocab $MODELDIR/pinit2 $MODELDIR/sgns_rand_pinit2 500 1 50
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit2.words
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit2.contexts
+./get_results_sgns_model.sh $UUID $MODELDIR/sgns_rand_pinit2 $RESDIR pinit2
+
+echo 'Pinit 2 has been evaluated'
+
+./create_word2vec_with_init.sh $MODELDIR/pairs $MODELDIR/counts.words.vocab $MODELDIR/pinit3 $MODELDIR/sgns_rand_pinit3 500 1 50
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit3.words
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit3.contexts
+./get_results_sgns_model.sh $UUID $MODELDIR/sgns_rand_pinit3 $RESDIR pinit3
+
+echo 'Pinit 3 has been evaluated'
+
+./create_word2vec_with_init.sh $MODELDIR/pairs $MODELDIR/counts.words.vocab $MODELDIR/svd.txt $MODELDIR/sgns_svd 500 1 50
+python hyperwords/text2numpy.py $MODELDIR/sgns_svd.words
+python hyperwords/text2numpy.py $MODELDIR/sgns_svd.contexts
+./get_results_sgns_model.sh $UUID $MODELDIR/sgns_svd $RESDIR svd
