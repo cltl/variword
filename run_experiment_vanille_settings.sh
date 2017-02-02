7d621eb9cbacdf6f366310723618654fa4b69a48
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-02-02 18:29:15 +0100

    other recent updates

--- run_experiment_vanille_settings.sh
+++ run_experiment_vanille_settings.sh
@@ -10,6 +10,13 @@ MODELDIR=$DIR/$UUID
 RESDIR=$3
 
 mkdir -p $MODELDIR
+mkdir -p $RESDIR$UUID
+
+
+#0 create setup.txt file with command that led to the results
+
+echo "./run_experiment_vanille_settings.sh $1 $2 $3" > $3$UUID/setup.txt
+
 
 #1. create pairs (win=2, dyn=None, sub=None) and counts
 ./clean_data_2_pairs_and_counts.sh $IN $MODELDIR 2
@@ -57,3 +64,38 @@ echo 'Pinit 3 has been evaluated'
 python hyperwords/text2numpy.py $MODELDIR/sgns_svd.words
 python hyperwords/text2numpy.py $MODELDIR/sgns_svd.contexts
 ./get_results_sgns_model.sh $UUID $MODELDIR/sgns_svd $RESDIR svd
+
+echo 'svd initiation has been evaluated'
+
+#creating reversed ordered corpus
+mv $MODELDIR/pairs $MODELDIR/pairs-orig
+tac $MODELDIR/pairs-orig > /data/pairs
+mv /data/pairs $MODELDIR/pairs
+
+# running word2vec with reversed data models
+./create_word2vec_with_init.sh $MODELDIR/pairs $MODELDIR/counts.words.vocab $MODELDIR/pinit1 $MODELDIR/sgns_rand_pinit1-rev 500 1 50
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit1-rev.words
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit1-rev.contexts
+./get_results_sgns_model.sh $UUID $MODELDIR/sgns_rand_pinit1-rev $RESDIR pinit1-rev
+
+
+echo 'Pinit 1 has been evaluated'
+
+./create_word2vec_with_init.sh $MODELDIR/pairs $MODELDIR/counts.words.vocab $MODELDIR/pinit2 $MODELDIR/sgns_rand_pinit2-rev 500 1 50
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit2-rev.words
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit2-rev.contexts
+./get_results_sgns_model.sh $UUID $MODELDIR/sgns_rand_pinit2-rev $RESDIR pinit2-rev
+
+echo 'Pinit 2 has been evaluated'
+
+./create_word2vec_with_init.sh $MODELDIR/pairs $MODELDIR/counts.words.vocab $MODELDIR/pinit3 $MODELDIR/sgns_rand_pinit3-rev 500 1 50
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit3-rev.words
+python hyperwords/text2numpy.py $MODELDIR/sgns_rand_pinit3-rev.contexts
+./get_results_sgns_model.sh $UUID $MODELDIR/sgns_rand_pinit3-rev $RESDIR pinit3-rev
+
+echo 'Pinit 3 has been evaluated'
+
+./create_word2vec_with_init.sh $MODELDIR/pairs $MODELDIR/counts.words.vocab $MODELDIR/svd.txt $MODELDIR/sgns_svd-rev 500 1 50
+python hyperwords/text2numpy.py $MODELDIR/sgns_svd-rev.words
+python hyperwords/text2numpy.py $MODELDIR/sgns_svd-rev.contexts
+./get_results_sgns_model.sh $UUID $MODELDIR/sgns_svd-rev $RESDIR svd-rev
