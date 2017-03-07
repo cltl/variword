7917a734c5e484d21151eab8b4667e035b77cf7c
Leon van Wissen <leon@vwissen.nl>
2017-03-07 18:44:41 +0100

    cosine distance for every word in vocabulary

--- evaluate_align/README.md
+++ evaluate_align/README.md
@@ -1,15 +1,27 @@
 
-forked and severely adapted from [histwords](https://github.com/williamleif/histwords) repo. 
+Forked and severely adapted from [histwords](https://github.com/williamleif/histwords) repo. It is unfortunately still using python 2.7...
 
-
-# how to use
+# How to use
 
 ```
 
     python2 align_models.py /path/to/models/ prefix
 
-    python2 ws_eval.py output/sgns_rand_pinit1.aligned simtestsets/ws/bruni_men.txt --type SGNS
+```
+
+## example
 
 ```
+    $ python2 align_models.py /home/leon/histwords/MODELS/wiki_100M_recom/run.2017-02-01-10:05:16 sgns_rand_pinit
+```
+
+# What does it do?
+
+The script first aligns the models that it finds in an directory, all starting with the specified prefix. Then it aligns them to each other and outputs them in the `output` folder. Then, it processes the entire vocabulary and outputs the cosine distance between the word in two embeddings. Values are written to `results.csv`.
+
+Similar vector spacing would return a distance of 1. Values are saved with a precision of 17 decimals, and that is also the standard representation in python2 when displaying a float. 
+
+Try to run it in an ipython notebook (e.g. `run align_models.py %arguments%`, then you can inspect dictionary `results` and pandas.df `df`. 
+
 
 
--- evaluate_align/align_models.py
+++ evaluate_align/align_models.py
@@ -11,7 +11,8 @@ import numpy as np
 import re
 from docopt import docopt
 from representations.embedding import Embedding
-from representations.sequentialembedding import SequentialEmbedding
+from itertools import combinations
+from collections import defaultdict
 
 def align_models(path, matchname, outfolder, add_context=True):
     """
@@ -20,29 +21,36 @@ def align_models(path, matchname, outfolder, add_context=True):
     :param matchname:
     :param outfolder:
     :param add_context:
-    :return:
+    :return: all aligned embeddings in a list of tuples (modelname, embedding).
     """
 
     vocabulary = []
     models = get_models(path, matchname)
+    print(models)
+
+    all_aligned_embeddings = []
 
     base_embedding = None
-    for n, model in enumerate(models):
-        print("Aligning ", model)
+    for n, modelname in enumerate(models):
+        print("Aligning ", modelname)
+
+        embedding_path = os.path.join(path, models[n])
 
         if n == 0: #  first run, nothing to align
-            aligned_embedding = Embedding.load(os.path.join(path, model), add_context=add_context)
+            aligned_embedding = Embedding.load(embedding_path, add_context=add_context)
         else:
-            embedding_path = os.path.join(path, models[n])
             embedding = Embedding.load(embedding_path, add_context=add_context)
-
             aligned_embedding = align(base_embedding, embedding)
 
         base_embedding = aligned_embedding
 
         #  Save it to the output dir!
-        save(aligned_embedding, model, outfolder)
+        save(aligned_embedding, modelname, outfolder)
         vocabulary.append(set(aligned_embedding.iw))
+
+        # But also add it to a list of embeddings for comparing the similarity between models.
+        # Then we don't have to load it again.
+        all_aligned_embeddings.append((modelname, aligned_embedding))
 
     common_vocab = set.intersection(*vocabulary)
     with open(os.path.join(outfolder, matchname + '.aligned.words.vocab'), 'w') as vocabfile:
@@ -50,7 +58,16 @@ def align_models(path, matchname, outfolder, add_context=True):
             vocabfile.write(i)
             vocabfile.write('\n')
 
+    return all_aligned_embeddings
+
 def save(aligned_embedding, model, outfolder):
+    """
+    Save the model and the vocabulary in the same way the hyperwords script does.
+    :param aligned_embedding: the aligned embedding (Embedding)
+    :param model: model name
+    :param outfolder: output folder
+    :return: None
+    """
 
     np.save(os.path.join(outfolder, model + '.aligned.words.npy'), aligned_embedding.m)
 
@@ -77,10 +94,57 @@ def align(embed1, embed2):
     Align embed 2 to embed1
     :return: aligned_embedding
     """
-
     aligned_embedding = alignment.smart_procrustes_align(embed1, embed2)
 
     return aligned_embedding
+
+def check_similarity(all_aligned_embeddings, vocab):
+    """
+    Check similarity for every word in the vocab between all models (aligned)
+    :param all_aligned_embeddings: list of tuples (modelname, embedding)
+    :param vocab:
+    :return: resultsdictionary. Every word is a key. Value is a dictionary of models and similarity.
+    """
+
+    results = []
+    resultsdictionary = defaultdict(dict)
+
+    with open(vocab) as vocabfile:
+        vocablist = vocabfile.read().split('\n')[:-1]
+
+    model_combinations = list(combinations(all_aligned_embeddings, 2))
+
+    for word in vocablist:
+        sims = []
+        for embeddingtuple in model_combinations:
+
+            models, embeddings = zip(*embeddingtuple)
+
+            sim = similarity(word, embeddings)
+            sims.append(sim)
+
+            resultsdictionary[word][models] = sim
+
+        results.append((word, sims))
+        # print(word, sims)
+
+    print(sorted(results, key=lambda x: x[1])[:20])
+    print('vocab', len(vocablist), 'results', len(results))
+
+    return resultsdictionary
+
+
+def similarity(word, embeddings):
+    """
+    Compute similarity for a word in two embeddings by taking the dot product.
+    :param embeddings: tuple of two embeddings
+    :return: similarity
+    """
+
+    emb1, emb2 = embeddings
+
+    sim = emb1.represent(word).dot(emb1.represent(word))
+    return sim
 
 if __name__ == "__main__":
 
@@ -88,8 +152,18 @@ if __name__ == "__main__":
 
     PATH = arguments['<folder>']
     MATCHNAME = arguments['<matchname>']
+    outfolder = 'output'
 
-    align_models(PATH, MATCHNAME, outfolder="output")
+    all_aligned_models = align_models(PATH, MATCHNAME, outfolder=outfolder)
+
+    default_vocab = os.path.join(outfolder, MATCHNAME + '.aligned.words.vocab')
+    results = check_similarity(all_aligned_models, vocab=default_vocab)
+
+    # make a panda out of it
+    import pandas as pd
+    df = pd.DataFrame.from_dict(results, orient='index')
+    df.to_csv(os.path.join(outfolder, 'results.csv'), float_format='%.17g') # 17 is the standard python2 notation
+
 
 
     # PATH = '/home/leon/Documenten/5.2_Machine_learning/3/histwords/MODELS/wiki_100M_recom/run.2017-02-01-10:05:16'
