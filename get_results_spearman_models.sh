af4b6c160268f71f46f7b5a16fccf68d2e534b71
cltlnl <cumeo89@gmail.com>
2017-03-28 14:09:09 +0200

    update spearman between models ws

--- get_results_spearman_models.sh
+++ get_results_spearman_models.sh
@@ -18,11 +18,24 @@ mkdir -p $RESULTSDIR/ws
 # init1 - init1-rev
 python hyperwords/ws_eval_models.py $wc results/$UUID/ws/ws353sim_wc_SGNSpinit1.tsv results/$UUID/ws/ws353sim_wc_SGNSpinit1-rev.tsv $RESULTSDIR/ws/ws353sim_wc_SGNS$suffix.tsv $UUIDRES
 
+# ws353_relatedness
 
-#python hyperwords/ws_eval.py $wc SGNS $RUNFILE testsets/ws/ws353_relatedness.txt $RESULTSDIR/ws/ws353rel_wc_SGNS$suffix.tsv $UUIDRES
+python hyperwords/ws_eval_models.py $wc results/$UUID/ws/ws353rel_wc_SGNSpinit1.tsv results/$UUID/ws/ws353rel_wc_SGNSpinit1-rev.tsv $RESULTSDIR/ws/ws353rel_wc_SGNS$suffix.tsv $UUIDRES
+
 
 # Bruni MEN
 
+python hyperwords/ws_eval_models.py $wc results/$UUID/ws/brunimen_wc_SGNSpinit1.tsv results/$UUID/ws/brunimen_wc_SGNSpinit1-rev.tsv $RESULTSDIR/ws/brunimen_wc_SGNS$suffix.tsv $UUIDRES
 
+
+# HILL simlex
+
+python hyperwords/ws_eval_models.py $wc results/$UUID/ws/hillsimlex_wc_SGNSpinit1.tsv results/$UUID/ws/hillsimlex_wc_SGNSpinit1-rev.tsv $RESULTSDIR/ws/hillsimlex_wc_SGNS$suffix.tsv $UUIDRES
+
+# Rare
+
+python hyperwords/ws_eval_models.py $wc results/$UUID/ws/luongrare_wc_SGNSpinit1.tsv results/$UUID/ws/luongrare_wc_SGNSpinit1-rev.tsv $RESULTSDIR/ws/luongrare_wc_SGNS$suffix.tsv $UUIDRES
 
 # Radinsky M Turk
+
+python hyperwords/ws_eval_models.py $wc results/$UUID/ws/radinskyturk_wc_SGNSpinit1.tsv results/$UUID/ws/radinskyturk_wc_SGNSpinit1-rev.tsv $RESULTSDIR/ws/radinskyturk_wc_SGNS$suffix.tsv $UUIDRES
